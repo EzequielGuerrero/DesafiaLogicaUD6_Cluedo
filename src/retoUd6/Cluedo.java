cca72d3488c2d8296080f2bcfae56f9c26ce0aa8
EzequielGuerrero <ezequiel_guerrero1@hotmail.com>
2024-03-19 20:01:53 +0100

    Añado metodo de actualiar arrays

--- src/retoUd6/Cluedo.java
+++ src/retoUd6/Cluedo.java
@@ -1,5 +1,7 @@
 package retoUd6;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cluedo {
@@ -13,7 +15,7 @@ public class Cluedo {
                 "Cuerda", "hacha", "Pesa", "Veneno", "Trofeo" };
 
         String lugares_Habitaciones[] = { "Casa de invitados", "Teatro", "Spa", "Observatorio", "Comedor", "Terraza",
-                "Salón", "Cocina", "Vestíbulo" };
+                "Salon", "Cocina", "Vestibulo" };
 
         menuJuego(personajes, armasPersonajes, lugares_Habitaciones);
 
@@ -24,25 +26,40 @@ public class Cluedo {
         switch (respuesta) {
             case 1:
                 System.out.println("¿Cuantos personajes quieres añadir?");
+
+                int numero = scn.nextInt();
+
+                actualizarArray(personajes, numero);
+
                 break;
 
             case 2:
                 System.out.println("¿Que arma quieres añadir?");
+                int numeroArmas = scn.nextInt();
+
+                actualizarArray(personajes, numeroArmas);
+
                 break;
 
             case 3:
 
                 System.out.println("¿Cual es el lugar o habitacion que quieres añadir?");
 
+                int numeroHB = scn.nextInt();
+
+                actualizarArray(lugares_Habitaciones, numeroHB);
+
                 break;
 
             case 4:
-
+                System.out.println("Hasta luego ;)");
                 break;
 
             default:
                 break;
         }
+
+        scn.close();
 
     }
 
@@ -83,6 +100,27 @@ public class Cluedo {
         System.out.println("2. Arma");
         System.out.println("3. Habitacion");
         System.out.println("4. Salir");
+
+    }
+
+    public static String[] actualizarArray(String[] arrayAntiguo, int num) {
+
+        Scanner scn = new Scanner(System.in);
+
+        String[] arrayNuevo = new String[arrayAntiguo.length + num];
+
+        System.out.println("Escribe lo que quieres añadir: ");
+        // intento de añadir los objetos al array:
+
+        String añadido = scn.nextLine();
+
+        System.arraycopy(arrayAntiguo, 0, arrayNuevo, 0, arrayAntiguo.length);
+
+        System.out.println("Nuevo array: " + Arrays.toString(arrayNuevo));
+
+        scn.close();
+
+        return arrayNuevo;
 
     }
 }
